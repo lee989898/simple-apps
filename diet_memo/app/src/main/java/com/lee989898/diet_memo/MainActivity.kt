c34312f3fe52b8f30570f436e6ea4febcba5bb88
lee989898 <wnsrud1352@gmail.com>
2021-12-04 22:57:25 +0900

    commit

---
+++ diet_memo/app/src/main/java/com/lee989898/diet_memo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.lee989898.diet_memo
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val writeButton = findViewById<ImageView>(R.id.writeBtn)
+        writeButton.setOnClickListener {
+
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this).setView(mDialogView).setTitle("운동 메모 다이얼로그")
+
+            val mAlertDialog = mBuilder.show()
+
+            val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            DateSelectBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val date : Int = today.get(Calendar.DATE)
+
+
+                val dlg = DatePickerDialog(this,object : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                        Log.d("MAIN", "${year}, ${month + 1}, ${dayOfMonth}")
+                        DateSelectBtn.setText("${year}, ${month + 1}, ${dayOfMonth}")
+                    }
+
+                }, year, month, date)
+                            dlg.show()}
+        }
+
+    }
+}
