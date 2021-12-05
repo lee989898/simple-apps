2bc0112901eeb38d71d581dd6e1f57aabf8f4f13
lee989898 <wnsrud1352@gmail.com>
2021-12-05 18:36:11 +0900

    commit

---
+++ diet_memo/app/src/main/java/com/lee989898/diet_memo/DataModel.kt
@@ -0,0 +1,7 @@
+package com.lee989898.diet_memo
+
+data class DataModel(
+    val date : String = "",
+    val memo : String = ""
+
+)
--- diet_memo/app/src/main/java/com/lee989898/diet_memo/MainActivity.kt
+++ diet_memo/app/src/main/java/com/lee989898/diet_memo/MainActivity.kt
@@ -1,22 +1,61 @@
 package com.lee989898.diet_memo
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.ImageView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        val dataModelList = mutableListOf<DataModel>()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val database = Firebase.database
+        val myRef = database.getReference("myMemo")
+
+        val listView = findViewById<ListView>(R.id.mainLV)
+
+        val adapter_list = ListViewAdapter(dataModelList)
+
+        listView.adapter = adapter_list
+
+        myRef.child(Firebase.auth.currentUser!!.uid).addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                dataModelList.clear()
+
+                for(dataModel in snapshot.children){
+                    Log.d("Data", dataModel.toString())
+                    dataModelList.add(dataModel.getValue(DataModel::class.java)!!)
+                }
+
+                adapter_list.notifyDataSetChanged()
+                Log.d("DataModel", dataModelList.toString())
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        )
+
         val writeButton = findViewById<ImageView>(R.id.writeBtn)
         writeButton.setOnClickListener {
 
@@ -26,6 +65,8 @@ class MainActivity : AppCompatActivity() {
             val mAlertDialog = mBuilder.show()
 
             val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            var dateText = ""
 
             DateSelectBtn?.setOnClickListener {
 
@@ -39,10 +80,27 @@ class MainActivity : AppCompatActivity() {
                     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
                         Log.d("MAIN", "${year}, ${month + 1}, ${dayOfMonth}")
                         DateSelectBtn.setText("${year}, ${month + 1}, ${dayOfMonth}")
+
+                        dateText = "${year}, ${month + 1}, ${dayOfMonth}"
                     }
 
                 }, year, month, date)
                             dlg.show()}
+            val saveBtn = mAlertDialog.findViewById<Button>(R.id.saveBtn)
+            saveBtn?.setOnClickListener {
+
+                val healMemo = mAlertDialog.findViewById<EditText>(R.id.healthMemo)?.text.toString()
+
+                val database = Firebase.database
+                val myRef = database.getReference("myMemo").child(Firebase.auth.currentUser!!.uid)
+
+                val model = DataModel(dateText, healMemo)
+
+                myRef.push().setValue(model)
+
+                mAlertDialog.dismiss()
+
+            }
         }
 
     }
