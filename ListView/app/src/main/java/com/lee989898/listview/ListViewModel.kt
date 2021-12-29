30b218a996d7514580f39527a9ed47a119714c6f
lee989898 <wnsrud1352@gmail.com>
2021-12-29 11:52:03 +0900

    commit

--- ListView/app/src/main/java/com/lee989898/listview/ListViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.lee989898.listview
-
-data class ListViewModel (
-    var title: String = "",
-    var content: String = ""
-
-        )
--- diet_memo/app/src/main/java/com/lee989898/diet_memo/DataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.lee989898.diet_memo
-
-data class DataModel(
-    val date : String = "",
-    val memo : String = ""
-
-)
