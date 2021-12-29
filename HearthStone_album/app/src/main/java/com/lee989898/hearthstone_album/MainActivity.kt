a137d75703558b5c58fc10c5a418b6b02cd3f445
lee989898 <wnsrud1352@gmail.com>
2021-12-29 12:51:29 +0900

    commit

--- HearthStone_album/app/src/main/java/com/lee989898/hearthstone_album/MainActivity.kt
+++ HearthStone_album/app/src/main/java/com/lee989898/hearthstone_album/MainActivity.kt
@@ -2,10 +2,20 @@ package com.lee989898.hearthstone_album
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val warriorImg = findViewById<ImageView>(R.id.main_warrior_iv)
+        warriorImg.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
